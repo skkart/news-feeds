d0663150e8521f19325458b3c9541da7c7cbb23f
skkart <skkarthik10@gmail.com>
2020-05-19 22:53:45 +0530

    Updates

---
+++ precache-manifest.d3b17b4566bde6514f87aa04509f859c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8788d5acfc47d0011ed2ad06467a811",
+    "url": "/news-feeds/index.html"
+  },
+  {
+    "revision": "bbacadfe8b744bf9f108",
+    "url": "/news-feeds/static/css/main.9dbed970.chunk.css"
+  },
+  {
+    "revision": "cb95e797c2a81cd646cc",
+    "url": "/news-feeds/static/js/2.06678edd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/news-feeds/static/js/2.06678edd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbacadfe8b744bf9f108",
+    "url": "/news-feeds/static/js/main.ccb953d0.chunk.js"
+  },
+  {
+    "revision": "1ae06f3826f2f19a683c",
+    "url": "/news-feeds/static/js/runtime-main.32bcb2b5.js"
+  }
+]);
